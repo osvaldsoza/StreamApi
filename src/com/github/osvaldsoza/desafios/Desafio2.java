06cd541dc84aa5acaccb1cd6ba43ba7d0734996f
osvaldsoza <osvald.soza@gmail.com>
2024-03-28 22:19:04 -0300

    first commit

---
+++ src/com/github/osvaldsoza/desafios/Desafio2.java
@@ -0,0 +1,17 @@
+package com.github.osvaldsoza.desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Desafio1 {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numeros.stream()
+                .sorted(Comparator.naturalOrder())
+                .distinct()
+                .forEach(System.out::println);
+    }
+}
---
+++ src/com/github/osvaldsoza/desafios/Desafio3.java
@@ -0,0 +1,20 @@
+package com.github.osvaldsoza.desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+//    Desafio 2 - Imprima a soma dos números pares da lista:
+//    Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+public class Desafio2 {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Integer totalSum = numeros.stream()
+                .filter(numero -> numero % 2 == 0)
+                .reduce(0, Integer::sum);
+
+        System.out.println(totalSum);
+    }
+}
